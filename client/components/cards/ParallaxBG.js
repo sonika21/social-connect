9d0b61648504e3bbf4c11fbc18270fef00923d51
Sonika Panigrahi <sonika.panigrahi@gmail.com>
2022-03-06 12:09:50 +0530

    Initial Commit

---
+++ client/components/cards/ParallaxBG.js
@@ -0,0 +1,25 @@
+const ParallaxBG = ({ url, children = "SOCIAL CONNECT" }) => {
+  return (
+    <div
+      className="container-fluid"
+      style={{
+        backgroundImage: "url( " + url + " )",
+        backgroundAttachment: "fixed",
+        padding: "100px 0px 75px 0px",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        display: "block",
+      }}
+    >
+      <h1
+        className="display-1 font-weight-bold text-center"
+        style={{ color: "#fff" }}
+      >
+        {children}
+      </h1>
+    </div>
+  );
+};
+
+export default ParallaxBG;
